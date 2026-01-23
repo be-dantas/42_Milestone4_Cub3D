9daa52e75f14fd77620248dee3ead76fbd8be101
wees-s <wesleyalexandre3946@gmail.com>
2026-01-23 17:32:19 -0300

    window init

---
+++ includes/cub3d.h
@@ -0,0 +1,20 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include "../minilibx-linux/mlx.h"
+
+# define WIDTH 800
+# define HEIGHT 600
+
+typedef struct s_access
+{
+	void	*mlx_connection;
+	void	*mlx_window;
+	void	*img;
+	char	*img_pointer;
+	int		bits_per_pixel;
+	int		line_len;
+	int		endian;
+}	t_access;
+
+#endif
--- main.c
+++ main.c
@@ -1,4 +1,19 @@
+#include "./includes/cub3d.h"
+
 int main()
 {
+    t_access access;
+
+    access.mlx_connection = mlx_init();
+
+    access.mlx_window = mlx_new_window
+    (
+        access.mlx_connection,
+        WIDTH,
+        HEIGHT,
+        "Cub3D - wedos-sa & bedantas"
+    );
+    
+    mlx_loop(access.mlx_connection);
     return (0);
 }
